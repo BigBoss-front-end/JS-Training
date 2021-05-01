5977b8c249bf7a1e8ec2913b326147b219de9efe
BigBoss <artembikbaev98@gmail.com>
2021-05-01 13:16:19 +0500

    created code on JS

---
+++ app.js
@@ -0,0 +1,10 @@
+var Tc = 22;
+var Tf = (9 / 5) * Tc + 32;
+var name = "Василий";
+var admin = name;
+var chemu = 'Чему будет равно JS-выражение 1000 + "108"?';
+var tomu = '1000108'
+
+alert(Tf, +' градусов по Фаренгейту');
+alert(admin);
+alert(chemu +'  Ответ - ' +tomu);
